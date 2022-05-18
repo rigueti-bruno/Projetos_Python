6ddfaabe89e3f56e15f6a3b26e96ebc977fba159
rigueti-bruno <bruno.rigueti@outlook.com>
2022-05-18 17:19:09 -0300

    inclusao de mais funções de impressao de piramide

--- imprimir_piramide/imprimir_piramide.py
+++ imprimir_piramide/imprimir_piramide.py
@@ -24,7 +24,34 @@ def piramide(n):
         print((" " * k) + ("*" * i * 2) + (" " * k) + "\n")
         k -= 1
         i += 1
+        
+# Essa função imprime um triangulo invertido alinhado à esquerda:
+def triangulo_esq_i(n):
+    k = 1
+    while n > 0:
+        print(("*" * n) + (" " * k) + "\n")
+        n -= 1
+        k += 1
+
+# Essa função imprime um triangulo invertido alinhado à direita:
+def triangulo_dir_i(n):
+    k = 1
+    while n > 0:
+        print((" " * k) + ("*" * n) + "\n")
+        n -= 1
+        k += 1
+
+# Essa função imprime uma pirâmide invertida:
+def piramide_i(n):
+    k = 1
+    while n > 0:
+        print((" " * k) + ("*" * n * 2) + (" " * k) + "\n")
+        n -= 1
+        k += 1
 
 triangulo_esq(8)
 triangulo_dir(8)
 piramide(8)
+triangulo_esq_i(8)
+triangulo_dir_i(8)
+piramide_i(8)
